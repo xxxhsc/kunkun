06de722912e2e33537374579b651b662e143dad0
swaggyH <937995148@qq.com>
2019-07-19 14:59:07 +0800

    Initial commit

---
+++ src/main/java/com/hsc/kunkun/KunkunApplication.java
@@ -0,0 +1,13 @@
+package com.hsc.kunkun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KunkunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KunkunApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/hsc/kunkun/entity/user.java
@@ -0,0 +1,13 @@
+package com.hsc.kunkun.entity;
+
+import javax.persistence.Entity;
+
+/**
+ * @Author: hsc
+ * @Description:
+ * @Date: 2019/7/19 14:55
+ */
+@Entity
+public class user {
+
+}
